d31c54e03d6c8daa2a61230121764b2ee3f66b60
Ahmed <elzek989@gmail.com>
2026-04-21 17:58:31 +0200

    Finish Register Screen Design and Add Validators Class.

--- lib/features/auth/views/register/register_screen.dart
+++ lib/features/auth/views/register/register_screen.dart
@@ -1,10 +1,97 @@
+import 'package:e_commerce/core/customized_widgets/customized_button.dart';
+import 'package:e_commerce/core/customized_widgets/customized_text_field.dart';
+import 'package:e_commerce/core/helpers/validator_helper.dart';
+import 'package:e_commerce/core/resources/app_assets.dart';
+import 'package:e_commerce/core/resources/app_colors.dart';
+import 'package:e_commerce/l10n/app_tr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(color: Colors.green);
+    final tr = LocalizationService.instance.tr;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30.w),
+          child: InkWell(
+            onTap: () {
+              context.pop();
+            },
+            child: Icon(Icons.arrow_back_ios),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                tr.create_account,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 34.sp,
+                  color: AppColors.black,
+                ),
+              ),
+              SizedBox(height: 45.h),
+              CustomizedTextField(
+                hintText: tr.name,
+                prefixIcon: AppIcons.profileIcon,
+                validator: Validators.requiredField,
+              ),
+              SizedBox(height: 22.h),
+              CustomizedTextField(
+                hintText: tr.phone,
+                prefixIcon: AppIcons.phoneIcon,
+                validator: Validators.requiredField,
+              ),
+              SizedBox(height: 22.h),
+              CustomizedTextField(
+                hintText: tr.email,
+                prefixIcon: AppIcons.emailIcon,
+                validator: Validators.email,
+              ),
+              SizedBox(height: 22.h),
+              CustomizedTextField(
+                hintText: tr.password,
+                prefixIcon: AppIcons.passwordIcon,
+                validator: Validators.password,
+                isPassword: true,
+              ),
+              SizedBox(height: 22.h),
+              CustomizedTextField(
+                hintText: tr.confirm,
+                prefixIcon: AppIcons.passwordIcon,
+                validator: Validators.password,
+                isPassword: true,
+              ),
+              SizedBox(height: 22.h,),
+              Text.rich(
+                TextSpan(
+                  style: TextStyle(fontSize: 12.sp, color: AppColors.grey),
+                  children: [
+                    TextSpan(text: tr.register_prompt_part1),
+                    TextSpan(
+                      text: tr.register,
+                      style: TextStyle(color: AppColors.red),
+                    ),
+                    TextSpan(text: tr.register_prompt_part2),
+                  ],
+                ),
+              ),
+              SizedBox(height: 28.h),
+              CustomizedButton(title: tr.register),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
