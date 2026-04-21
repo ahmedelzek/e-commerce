cf90b45e0f8af0009f1da342c6b8f3e2b4d3bc52
Ahmed <elzek989@gmail.com>
2026-04-21 17:58:57 +0200

    Finish Register Screen Design and Add Validators Class.

--- lib/features/auth/views/login/login_screen.dart
+++ lib/features/auth/views/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/core/customized_widgets/customized_button.dart';
 import 'package:e_commerce/core/customized_widgets/customized_text_field.dart';
+import 'package:e_commerce/core/helpers/validator_helper.dart';
 import 'package:e_commerce/core/resources/app_assets.dart';
 import 'package:e_commerce/core/resources/app_colors.dart';
 import 'package:e_commerce/l10n/app_tr.dart';
@@ -42,11 +43,13 @@ class LoginScreen extends StatelessWidget {
             CustomizedTextField(
               hintText: tr.email,
               prefixIcon: AppIcons.emailIcon,
+              validator: Validators.email,
             ),
             SizedBox(height: 22.h),
             CustomizedTextField(
               hintText: tr.password,
               prefixIcon: AppIcons.passwordIcon,
+              validator: Validators.password,
               isPassword: true,
             ),
             SizedBox(height: 56.h),
